c0ed58809a2209e1ba3751bbe52b05134ecaac7c
Rita Trindade da Cruz <ritatrcr@unisabana.edu.co>
2024-11-03 05:16:54 -0500

    fix index

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,3 @@ export { default as ProtectedRoute } from './navigation/protectedRoute';
 export { default as UnprotectedRoute } from './navigation/unprotectedRoute';
 export { default as SidePanel } from './shared/sidePanel';
 export { default as ChatMessage } from './designStyles/chatMessage'  
-export { default as FileCard } from './designStyles/fileCard'  
